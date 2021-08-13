162328516031a8f218225bab64be697ce8407ce1
Leonardo Melo <leonardomelo01@gmail.com>
2021-08-12 22:04:51 -0300

    Finished incluir Pet method

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -9,6 +9,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.contains;
 
 public class Pet {
 
@@ -31,6 +33,10 @@ public class Pet {
         .then()
                 .log().all()
                 .statusCode(200)
+                .body("name", is("Snoop"))
+                .body("status", is("available"))
+                .body("category.name", is("dog"))
+                .body("tags.name", contains("sta"))
         ;
     }
 }
